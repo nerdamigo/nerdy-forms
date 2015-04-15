edc9365b48080c74214467090175094060d25f1b
matt <matt@nerdamigo.com>
2015-04-14 22:33:53 -0400

    build bump

--- Nerdamigo.NerdyForms/Properties/AssemblyInfo.cs
+++ Nerdamigo.NerdyForms/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nerdamigo.NerdyForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Quick and Easy handling of HTML forms for ASP.Net MVC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("NerdAmigo")]
 [assembly: AssemblyProduct("Nerdamigo.NerdyForms")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
